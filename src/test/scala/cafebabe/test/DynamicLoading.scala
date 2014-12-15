1f1884ae69195a89b0c8ed8ca4f210036c4e350b
Trugath <elliot.stirling@gmail.com>
2014-12-15 21:18:49 +0000

    Modified so DL 2 shows usage of dynObj.plusOne(41) === 42

--- src/test/scala/cafebabe/test/DynamicLoading.scala
+++ src/test/scala/cafebabe/test/DynamicLoading.scala
@@ -1,40 +1,48 @@
-package cafebabe.test
 
-import cafebabe._
-import cafebabe.ByteCodes._
-import cafebabe.AbstractByteCodes._
+package cafebabe.test {
 
-import org.scalatest.FunSuite
-class DynamicLoading extends FunSuite {
-  private def mkMinimalClassFile : ClassFile = {
-    val cf = new ClassFile("MyTest", None)
-    cf.addDefaultConstructor()
-    val ch = cf.addMethod("I", "plusOne", "I").codeHandler
-    ch << ILoad(1) << Ldc(1) << IADD << IRETURN
-    ch.freeze
-    cf
+  abstract class MyTestBase {
+    def plusOne( i: Int ): Int
   }
 
-  test("DL 1") {
-    val cf = mkMinimalClassFile
+  import cafebabe._
+  import cafebabe.ByteCodes._
+  import cafebabe.AbstractByteCodes._
 
-    val cl = new CafebabeClassLoader
-    cl.register(cf)
-    
-    val c = cl.loadClass("MyTest")
-    val o = c.newInstance().asInstanceOf[AnyRef]
-    val m = c.getMethod("plusOne", Integer.TYPE)
+  import org.scalatest.FunSuite
 
-    (m.invoke(o, 41 : java.lang.Integer) === 42)
-  }
+  class DynamicLoading extends FunSuite {
 
-  test("DL 2") {
-    // NB this test is incomplete.
-    val cf = mkMinimalClassFile
+    private def mkMinimalClassFile: ClassFile = {
+      val cf = new ClassFile("cafebabe$test$MyTest", Some("cafebabe/test/MyTestBase"))
+      cf.addDefaultConstructor()
+      val ch = cf.addMethod("I", "plusOne", "I").codeHandler
+      ch << ILoad(1) << Ldc(1) << IADD << IRETURN
+      ch.freeze
+      cf
+    }
 
-    val cl = new CafebabeClassLoader
-    cl.register(cf)
-    val dynObj = cl.newInstance("MyTest")
-    // dynObj.plusOne(41) would ideally work...
+    test("DL 1") {
+      val cf = mkMinimalClassFile
+
+      val cl = new CafebabeClassLoader
+      cl.register(cf)
+
+      val c = cl.loadClass("cafebabe$test$MyTest")
+      val o = c.newInstance().asInstanceOf[AnyRef]
+      val m = c.getMethod("plusOne", Integer.TYPE)
+
+      assert(m.invoke(o, 41: java.lang.Integer) === 42)
+    }
+
+    test("DL 2") {
+      // NB this test is incomplete.
+      val cf = mkMinimalClassFile
+
+      val cl = new CafebabeClassLoader
+      cl.register(cf)
+      val dynObj = cl.newInstance("cafebabe$test$MyTest").asInstanceOf[MyTestBase]
+      assert(dynObj.plusOne(41) === 42)
+    }
   }
 }

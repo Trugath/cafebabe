e11fa072437b69960e46d401e67942d783f3fea7
Trugath <elliot.stirling@gmail.com>
2014-12-15 22:38:04 +0000

    clean package layout

--- src/test/scala/cafebabe/test/DynamicLoading.scala
+++ src/test/scala/cafebabe/test/DynamicLoading.scala
@@ -1,47 +1,45 @@
+package cafebabe.test
 
-package cafebabe.test {
+abstract class MyTestBase {
+  def plusOne( i: Int ): Int
+}
 
-  abstract class MyTestBase {
-    def plusOne( i: Int ): Int
+import cafebabe._
+import cafebabe.ByteCodes._
+import cafebabe.AbstractByteCodes._
+
+import org.scalatest.FunSuite
+
+class DynamicLoading extends FunSuite {
+
+  private def mkMinimalClassFile: ClassFile = {
+    val cf = new ClassFile("MyTest", Some("cafebabe/test/MyTestBase"))
+    cf.addDefaultConstructor()
+    val ch = cf.addMethod("I", "plusOne", "I").codeHandler
+    ch << ILoad(1) << Ldc(1) << IADD << IRETURN
+    ch.freeze
+    cf
   }
 
-  import cafebabe._
-  import cafebabe.ByteCodes._
-  import cafebabe.AbstractByteCodes._
+  test("DL 1") {
+    val cf = mkMinimalClassFile
 
-  import org.scalatest.FunSuite
+    val cl = new CafebabeClassLoader
+    cl.register(cf)
 
-  class DynamicLoading extends FunSuite {
+    val c = cl.loadClass("MyTest")
+    val o = c.newInstance().asInstanceOf[AnyRef]
+    val m = c.getMethod("plusOne", Integer.TYPE)
 
-    private def mkMinimalClassFile: ClassFile = {
-      val cf = new ClassFile("MyTest", Some("cafebabe/test/MyTestBase"))
-      cf.addDefaultConstructor()
-      val ch = cf.addMethod("I", "plusOne", "I").codeHandler
-      ch << ILoad(1) << Ldc(1) << IADD << IRETURN
-      ch.freeze
-      cf
-    }
+    assert(m.invoke(o, 41: java.lang.Integer) === 42)
+  }
 
-    test("DL 1") {
-      val cf = mkMinimalClassFile
+  test("DL 2") {
+    val cf = mkMinimalClassFile
 
-      val cl = new CafebabeClassLoader
-      cl.register(cf)
-
-      val c = cl.loadClass("MyTest")
-      val o = c.newInstance().asInstanceOf[AnyRef]
-      val m = c.getMethod("plusOne", Integer.TYPE)
-
-      assert(m.invoke(o, 41: java.lang.Integer) === 42)
-    }
-
-    test("DL 2") {
-      val cf = mkMinimalClassFile
-
-      val cl = new CafebabeClassLoader
-      cl.register(cf)
-      val dynObj = cl.newInstance("MyTest").asInstanceOf[MyTestBase]
-      assert(dynObj.plusOne(41) === 42)
-    }
+    val cl = new CafebabeClassLoader
+    cl.register(cf)
+    val dynObj = cl.newInstance("MyTest").asInstanceOf[MyTestBase]
+    assert(dynObj.plusOne(41) === 42)
   }
 }

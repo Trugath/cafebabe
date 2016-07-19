2bcb7291cbc84cf995b4690992459210172441db
Elliot Stirling <elliot.stirling@gmail.com>
2016-07-19 16:22:11 +0100

    DynObj is now fit for consumption

--- src/main/scala/cafebabe/DynObj.scala
+++ src/main/scala/cafebabe/DynObj.scala
@@ -4,11 +4,11 @@ import scala.collection.mutable.{Map=>MutableMap}
 
 import scala.language.dynamics
 
-/** Do not rely on this object, as it will change/be renamed, etc.
- * The goal is for `CafebabeClassLoader`s to generate `DynObj`s or
- * equivalent, so that users of dynamic class generation have to
- * write explicit reflection code as little as possible. */
-class DynObj private[cafebabe](base : Any) extends Dynamic {
+/** `CafebabeClassLoader`s generate `DynObj`s so that users
+  * of dynamic class generation have to write explicit reflection
+  * code as little as possible.
+  */
+class DynObj private[cafebabe] (base: Any) extends Dynamic {
 
   import reflect.runtime.{universe => ru}
   import ru._
